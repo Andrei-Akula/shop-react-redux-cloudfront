1ac23031831748630ceba5589859e15f6b4c8820
Akula, Andrei <andrei.akula@mckesson.com>
2025-07-05 17:46:35 +0300

    ImportServiceStack refactoring

--- cdk/lib/stacks/ImportServiceStack/import-service-stack.ts
+++ cdk/lib/stacks/ImportServiceStack/import-service-stack.ts
@@ -8,7 +8,7 @@ import * as aws_s3_deployment from 'aws-cdk-lib/aws-s3-deployment';
 import * as aws_s3_notifications from 'aws-cdk-lib/aws-s3-notifications';
 import * as iam from 'aws-cdk-lib/aws-iam';
 
-const productsAndStockWritePoliicy = new iam.PolicyStatement({
+const productsAndStockWritePolicy = new iam.PolicyStatement({
   actions: ["dynamodb:PutItem"],
   resources: [
     cdk.Fn.importValue("ProductsTableArn"),
@@ -26,141 +26,166 @@ const commonResponseRaparameters = {
   'method.response.header.Access-Control-Allow-Methods': true,
 };
 
-const commmonLambdaProps = {
+const commonLambdaProps = {
   runtime: lambda.Runtime.NODEJS_20_X,
   memorySize: 1024,
   timeout: cdk.Duration.seconds(5),
   code: lambda.Code.fromAsset(path.join(__dirname, '../../lambdas/import-products-file-handler')),
 }
 
+function createImportApiEndpoint(scope: Construct, importProductsFileFunction: lambda.Function) {
+  const api = new apigateway.RestApi(scope, 'import-api', {
+    restApiName: 'Import API Gateway',
+  });
+
+  const importResource = api.root.addResource('import');
+
+  importResource.addCorsPreflight({
+    allowOrigins: ['*'],
+    allowMethods: ['GET']
+  });
+
+  // lambda integration
+  const importProductsFileIntegration = new apigateway.LambdaIntegration(importProductsFileFunction, {
+    integrationResponses: [
+      {
+        statusCode: '200',
+        responseParameters: commonIntegrationGetResponseParameters,
+      },
+      {
+        statusCode: '400',
+        selectionPattern: '.+is required',
+        responseParameters: commonIntegrationGetResponseParameters,
+      },
+      {
+        statusCode: '500',
+        selectionPattern: '.+', // Catch-all for any other error
+        responseParameters: commonIntegrationGetResponseParameters,
+      }
+    ],
+    requestTemplates: {
+      'application/json': `{ "fileName": "$input.params('fileName')" }`,
+    },
+    passthroughBehavior: apigateway.PassthroughBehavior.NEVER,
+    proxy: false,
+  });
+
+  importResource.addMethod('GET', importProductsFileIntegration, {
+    methodResponses: [
+      {
+        statusCode: '200',
+        responseParameters: commonResponseRaparameters,
+        responseModels: {
+          'application/json': apigateway.Model.EMPTY_MODEL,
+        },
+      },
+      {
+        statusCode: '400',
+        responseParameters: commonResponseRaparameters,
+        responseModels: {
+          'application/json': apigateway.Model.ERROR_MODEL,
+        }
+      },
+      {
+        statusCode: '500',
+        responseParameters: commonResponseRaparameters,
+        responseModels: {
+          'application/json': apigateway.Model.ERROR_MODEL,
+        }
+      }
+    ]
+  });
+
+  return api;
+}
+
+function createUploadedBucket(scope: Construct) {
+  const uploadedBucket = new aws_s3.Bucket(scope, 'UploadedBucket', {
+    autoDeleteObjects: true,
+    blockPublicAccess: new aws_s3.BlockPublicAccess({ restrictPublicBuckets: true }),
+    removalPolicy: cdk.RemovalPolicy.DESTROY,
+    cors: [{
+      allowedMethods: [aws_s3.HttpMethods.GET, aws_s3.HttpMethods.PUT],
+      allowedOrigins: ['*'],
+      allowedHeaders: ['*'],
+    }],
+    lifecycleRules: [{
+      id: 'DeleteUploadedFiles',
+      enabled: true,
+      prefix: 'uploaded/',
+      expiration: cdk.Duration.days(1), // Automatically delete files after 1 day
+    }],
+    versioned: false,
+  });
+
+  uploadedBucket.addToResourcePolicy(new cdk.aws_iam.PolicyStatement({
+    actions: ['s3:PutObject'],
+    resources: [`${uploadedBucket.bucketArn}/uploaded/*`],
+    principals: [new cdk.aws_iam.AnyPrincipal()],
+  }));
+
+  new cdk.CfnOutput(scope, 'UploadedBucketName', {
+    value: uploadedBucket.bucketName,
+    description: 'The name of UploadedBucket',
+    exportName: 'UploadedBucketName',
+  });
+
+  return uploadedBucket;
+}
+
+function createImportFileParserFunction(scope: Construct, lambdaEnv: { [key: string]: string }) {
+  return new lambda.Function(scope, 'import-file-parser-function', {
+    ...commonLambdaProps,
+    handler: 'import-products-file-handler.importFileParser',
+    environment: lambdaEnv,
+  });
+}
+
+function setupImportFileParseFunction(importFileParserFunction: lambda.Function) {
+  // Grant Lambda permissions to write to DynamoDB
+  importFileParserFunction.addToRolePolicy(productsAndStockWritePolicy);
+}
+
+function addS3EventHandling(uploadedBucket: aws_s3.Bucket, importFileParserFunction: lambda.Function) {
+  // Grant Lambda permissions to read from S3
+  uploadedBucket.grantRead(importFileParserFunction);
+
+  // Add S3 notification for the uploaded/ prefix
+  uploadedBucket.addEventNotification(
+    aws_s3.EventType.OBJECT_CREATED,
+    new aws_s3_notifications.LambdaDestination(importFileParserFunction),
+    {
+      prefix: 'uploaded/', // Only trigger for objects in the 'uploaded/' prefix);
+    }
+  );
+}
+
+function createImportProductsFileFunction(scope: Construct, lambdaEnv: { [key: string]: string }) {
+  return new lambda.Function(scope, 'import-products-file-function', {
+    ...commonLambdaProps,
+    handler: 'import-products-file-handler.importProductsFile',
+    environment: lambdaEnv,
+  });
+}
+
 export class ImportServiceStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+    
+    const uploadedBucket = createUploadedBucket(this);
 
-    // bucket
-    const uploadedBucket = new aws_s3.Bucket(this, 'UploadedBucket', {
-      autoDeleteObjects: true,
-      blockPublicAccess: new aws_s3.BlockPublicAccess({ restrictPublicBuckets: true }),
-      removalPolicy: cdk.RemovalPolicy.DESTROY,
-      cors: [{
-        allowedMethods: [aws_s3.HttpMethods.GET, aws_s3.HttpMethods.PUT],
-        allowedOrigins: ['*'],
-        allowedHeaders: ['*'],
-      }],
-      lifecycleRules: [{
-        id: 'DeleteUploadedFiles',
-        enabled: true,
-        prefix: 'uploaded/',
-        expiration: cdk.Duration.days(1), // Automatically delete files after 1 day
-      }],
-      versioned: false,
-    });
-
-    uploadedBucket.addToResourcePolicy(new cdk.aws_iam.PolicyStatement({
-      actions: ['s3:PutObject'],
-      resources: [`${uploadedBucket.bucketArn}/uploaded/*`],
-      principals: [new cdk.aws_iam.AnyPrincipal()],
-    }));
-
-    new cdk.CfnOutput(this, 'UploadedBucketName', {
-      value: uploadedBucket.bucketName,
-      description: 'The name of UploadedBucket',
-      exportName: 'UploadedBucketName',
-    });
-
-    // lambdas
     const commonLambdaEnvironment = {
       UPLOADED_BUCKET_NAME: uploadedBucket.bucketName,
     };
 
-    const importProductsFileFunction = new lambda.Function(this, 'import-products-file-function', {
-      ...commmonLambdaProps,
-      handler: 'import-products-file-handler.importProductsFile',
-      environment: commonLambdaEnvironment,
-    });
+    const importFileParserFunction = createImportFileParserFunction(this, commonLambdaEnvironment);
+    setupImportFileParseFunction(importFileParserFunction);
+    addS3EventHandling(uploadedBucket, importFileParserFunction);
 
-    const importFileParserFunction = new lambda.Function(this, 'import-file-parser-function', {
-      ...commmonLambdaProps,
-      handler: 'import-products-file-handler.importFileParser',
-      environment: commonLambdaEnvironment,
-    });
-
-    // Grant Lambda permissions to write to DynamoDB
-    importFileParserFunction.addToRolePolicy(productsAndStockWritePoliicy);
-
-    // Grant Lambda permissions to read from S3
-    uploadedBucket.grantRead(importFileParserFunction);
-
-     // Add S3 notification for the uploaded/ prefix
-    uploadedBucket.addEventNotification(
-      aws_s3.EventType.OBJECT_CREATED, 
-      new aws_s3_notifications.LambdaDestination(importFileParserFunction),
-      {
-        prefix: 'uploaded/', // Only trigger for objects in the 'uploaded/' prefix);
-      }
-    );
+    const importProductsFileFunction = createImportProductsFileFunction(this, commonLambdaEnvironment);
 
     // API Gateway
-    const api = new apigateway.RestApi(this, 'import-api', {
-      restApiName: 'Import API Gateway',
-    });
-
-    const importResource = api.root.addResource('import');
-
-    importResource.addCorsPreflight({
-      allowOrigins: ['*'],
-      allowMethods: ['GET']
-    });
-
-    // lambda integration
-    const importProductsFileIntegration = new apigateway.LambdaIntegration(importProductsFileFunction, {
-      integrationResponses: [
-        {
-          statusCode: '200',
-          responseParameters: commonIntegrationGetResponseParameters,
-        },
-        {
-          statusCode: '400',
-          selectionPattern: '.+is required',
-          responseParameters: commonIntegrationGetResponseParameters,
-        },
-        {
-          statusCode: '500',
-          selectionPattern: '.+', // Catch-all for any other error
-          responseParameters: commonIntegrationGetResponseParameters,
-        }
-      ],
-      requestTemplates: {
-        'application/json': `{ "fileName": "$input.params('fileName')" }`,
-      },
-      passthroughBehavior: apigateway.PassthroughBehavior.NEVER,
-      proxy: false,
-    });
-
-    importResource.addMethod('GET', importProductsFileIntegration, {
-      methodResponses: [
-        {
-          statusCode: '200',
-          responseParameters: commonResponseRaparameters,
-          responseModels: {
-            'application/json': apigateway.Model.EMPTY_MODEL,
-          },
-        },
-        {
-          statusCode: '400',
-          responseParameters: commonResponseRaparameters,
-          responseModels: {
-            'application/json': apigateway.Model.ERROR_MODEL,
-          }
-        },
-        {
-          statusCode: '500',
-          responseParameters: commonResponseRaparameters,
-          responseModels: {
-            'application/json': apigateway.Model.ERROR_MODEL,
-          }
-        }
-      ]
-    });
+    createImportApiEndpoint(this, importProductsFileFunction); 
   }
 }
+
